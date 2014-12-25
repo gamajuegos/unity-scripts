fdc872dc4e667fc26a0c769720c4415c5ac6d60f
Gamajuegos <juegosbs@gmail.com>
2014-12-25 10:30:54 -0430

    creating camera scripts

---
+++ camera/ROTATION/SmoothZoomCamera.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothZoomCamera : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float distance = 20.0f;
+    public float zoomSpeed = 2.0f;
+    public float smooth = 240.0f;
+    private const int zoomMin = 0;
+    private const int zoomMax = 1;
+   
+    void Start()
+    {
+        // Make the rigid body not change rotation
+        if (rigidbody)
+            rigidbody.freezeRotation = true;
+      
+    }
+    void ZoomIn()
+    {
+        camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, distance, Time.deltaTime * zoomSpeed);
+    }
+
+
+    void OnGUI() 
+    {
+       var e = Event.current;
+       
+       if (e.type == EventType.ScrollWheel)
+       {
+         bool targetFOV = (float)Input.GetAxis("Mouse ScrollWheel") > 0;
+         if (!targetFOV)
+             distance += zoomMax * smooth * 0.02f;
+         else
+             distance -= zoomMax * smooth * 0.02f;
+       }
+        
+    }
+    void LateUpdate()
+    {
+         if (target)
+        {
+           ZoomIn();
+        }
+      
+    }
+
+
+}
